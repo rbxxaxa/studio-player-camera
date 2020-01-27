61d95dc17351f94e05317ac8996adf919c7674f3
rbxxaxa <rbx.xaxa@gmail.com>
2020-01-26 17:37:39 -0800

    Initialize page

--- Plugin/Core/Components/App/RunningView/init.lua
+++ Plugin/Core/Components/App/RunningView/init.lua
@@ -11,8 +11,12 @@ local getMainManager = ContextGetter.getMainManager
 
 local Components = Plugin.Core.Components
 local Foundation = Components.Foundation
-local TabbedMenu = require(Foundation.TabbedMenu)
-local Test = require(script.Test)
+local ThemedTextButton = require(Foundation.ThemedTextButton)
+local ScrollingVerticalList = require(Foundation.ScrollingVerticalList)
+local VerticalList = require(Foundation.VerticalList)
+local NumericalSliderField = require(Foundation.NumericalSliderField)
+local CheckboxField = require(Foundation.CheckboxField)
+
 local RunningView = Roact.PureComponent:extend("RunningView")
 
 function RunningView:init()
@@ -26,8 +30,7 @@ end
 function RunningView:render()
 	return withTheme(
 		function(theme)
-			local page =
-				Roact.createElement(
+			local page = Roact.createElement(
 				"Frame",
 				{
 					BackgroundColor3 = theme.mainBackgroundColor,
@@ -35,22 +38,73 @@ function RunningView:render()
 					Size = UDim2.new(1, 0, 1, 0)
 				},
 				{
-					Tabber = Roact.createElement(
-						TabbedMenu,
+					Roact.createElement(
+						ScrollingVerticalList,
 						{
-							tabs = {
-								{id = "Test", text = "Test", image = ""}
-							},
-							activeId = self.state.currentTab,
-							onTabClick = function(tabId)
-								if self.state.currentTab ~= tabId then
-									self:setState {currentTab = tabId}
-									getMainManager(self):Deactivate()
-								end
-							end
+							Size = UDim2.new(1, 0, 1, 0),
 						},
 						{
-							Test = self.state.currentTab == "Test" and Roact.createElement(Test)
+							List = Roact.createElement(
+								VerticalList,
+								{
+									width = UDim.new(1, 0),
+									PaddingLeftPixel = 4,
+									PaddingRightPixel = 4,
+									PaddingTopPixel = 4,
+									PaddingBottomPixel = 4,
+									ElementPaddingPixel = 4,
+								},
+								{
+									MainButton = Roact.createElement(
+										ThemedTextButton,
+										{
+											Text = "Spawn Player Camera",
+											Size = UDim2.new(1, 0, 0, 60),
+											LayoutOrder = 1
+										}
+									),
+									WalkSpeed = Roact.createElement(
+										NumericalSliderField,
+										{
+											label = "WalkSpeed",
+											minValue = 1,
+											maxValue = 100,
+											value = 16,
+											valueSnap = 1,
+											valueRound = 1,
+											LayoutOrder = 2
+										}
+									),
+									JumpPower = Roact.createElement(
+										NumericalSliderField,
+										{
+											label = "JumpPower",
+											minValue = 1,
+											maxValue = 200,
+											value = 50,
+											valueSnap = 1,
+											valueRound = 1,
+											LayoutOrder = 3
+										}
+									),
+									WalkThroughWalls = Roact.createElement(
+										CheckboxField,
+										{
+											label = "Walk through walls?",
+											checked = true,
+											LayoutOrder = 4
+										}
+									),
+									SimulateGravity = Roact.createElement(
+										CheckboxField,
+										{
+											label = "Simulate gravity?",
+											checked = true,
+											LayoutOrder = 5
+										}
+									)
+								}
+							)
 						}
 					)
 				}
